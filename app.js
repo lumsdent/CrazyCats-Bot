69e777fdc38a77bf6b2ac061572fe3986af9b911
lumsdent <tyler.j.lumsden@gmail.com>
2020-08-26 18:54:09 -0400

    Some refactor and added gain xp command

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import { Client, Collection } from "discord.js";
+import { Client } from "discord.js";
 
 import config from "./config/config.js";
-import { createProfile } from "./user/UserService.js";
-
+import { createProfile, getProfile, updateXP } from "./user/UserService.js";
+import User from "./User.js";
 const client = new Client();
 
 client.once("ready", () => {
@@ -12,21 +12,33 @@ client.once("ready", () => {
 client.on("message", (message) => {
   if (message.content === "+start") {
     go(message);
+  } else if (message.content === "+profile") {
+    next(message);
+  } else if (message.content === "+gain") {
+    gain(message);
   }
 });
 
 async function go(message) {
-  const result = await createProfile({
-    discordId: message.author.id,
-    name: message.author.username,
-    level: 1,
-    xp: 0,
-  });
-  let [user] = result.ops;
-  let { name, level, xp } = user;
-
-  message.channel.send(
-    `Profile created! You are ${name}.  Begin your adventure at Level ${level}, ${xp}XP`
-  );
+  const result = await createProfile(new User(message));
+  if (result.newUser) {
+    const { newUser } = result;
+    message.channel.send(
+      `Profile created! You are ${newUser.name}.  Begin your adventure at Level ${newUser.level}, ${newUser.xp}XP`
+    );
+  } else {
+    message.channel.send(`Profile already exists`);
+  }
 }
+
+async function next(message) {
+  const { name, level, xp } = await getProfile(message.author.id);
+  message.channel.send(`Profile: Name: ${name}, Level: ${level}, ${xp}XP`);
+}
+
+async function gain(message) {
+  const { randomBeast, gainedXp } = await updateXP(message.author.id);
+  message.channel.send(`You gained ${gainedXp} XP by killing a ${randomBeast}`);
+}
+
 client.login(config.token);
--- user/UserService.js
+++ user/UserService.js
@@ -1,23 +1,48 @@
 import mongodb from "mongodb";
 const { MongoClient } = mongodb;
 
+import beastiary from "../beastiary.js";
 import config from "../config/config.js";
 
-const client = new MongoClient(config.dbConnectionString, {
-  useUnifiedTopology: true,
-});
+export async function createProfile(user) {
+  //if profile exists, exit
+  const existingUser = await getProfile(user.discordId);
+  if (existingUser) return { existingUser };
 
-export async function createProfile(newProfile) {
+  const client = getMDBClient();
+  try {
+    await client.connect();
+
+    const results = await client
+      .db("cc_sandbox")
+      .collection("users")
+      .insertOne(user);
+    console.log(
+      `New profile created with the following id: ${results.insertedId}`
+    );
+    let newUser = results.ops.shift();
+    return { newUser };
+  } catch (e) {
+    console.error("uh oh!");
+  } finally {
+    await client.close();
+  }
+}
+
+export async function getProfile(discordId) {
+  const client = getMDBClient();
   try {
     await client.connect();
 
     const result = await client
       .db("cc_sandbox")
       .collection("users")
-      .insertOne(newProfile);
-    console.log(
-      `New profile created with the following id: ${result.insertedId}`
-    );
+      .findOne({ discordId: discordId });
+    if (result) {
+      console.log(
+        `Profile found => Name: ${result.name}, Level: ${result.level}, ${result.xp}XP`
+      );
+    }
     return result;
   } catch (e) {
     console.error(e);
@@ -26,20 +51,40 @@ export async function createProfile(newProfile) {
   }
 }
 
-export async function getProfile(discordId) {
+export async function updateXP(discordId) {
+  const gainedXp = getRandomInt(10);
+  console.log(`${gainedXp} gained`);
+  const { xp: currentXp, level: level } = await getProfile(discordId);
+  console.log(level);
+  const beastArr = beastiary[level];
+  console.log(beastArr);
+  const randomBeast = beastArr[getRandomFromArr(beastArr)];
+  console.log(randomBeast);
+  const client = getMDBClient();
+  const newXp = { xp: currentXp + gainedXp };
   try {
     await client.connect();
-
     const result = await client
       .db("cc_sandbox")
       .collection("users")
-      .findOne({ discordId: discordId });
-    console.log(
-      `New profile created with the following id: ${result.insertedId} ${result.discordId} ${result.name}`
-    );
+      .updateOne({ discordId: discordId }, { $set: newXp });
+    return { randomBeast, gainedXp };
   } catch (e) {
     console.error(e);
   } finally {
     await client.close();
   }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max) + 1);
+}
+
+function getRandomFromArr(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+function getMDBClient() {
+  return new MongoClient(config.dbConnectionString, {
+    useUnifiedTopology: true,
+  });
 }
